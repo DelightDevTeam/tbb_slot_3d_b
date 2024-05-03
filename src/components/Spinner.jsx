a737b5068eb3f4ffc0bb053ba2c69aec1cb64974
mtk303 <maythinkyiedu@gmail.com>
2024-05-03 21:07:52 +0630

    upt spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <div className=''>
         <div className="spinner-border text-danger spinner-border-sm" role="status">
-            <span className="visually-hidden">Loading...</span>
+            <span className="visually-hidden">Loading....</span>
         </div>
     </div>
   )
